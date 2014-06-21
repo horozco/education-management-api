d25e7dbc04bf2db6c16077d56c6bde410c936f4a
Edgar Ortega <edgar.ortega@koombea.com>
2014-06-21 12:10:10 -0500

    Add Coaches

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612214657) do
+ActiveRecord::Schema.define(version: 20140621165019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coaches", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.integer  "organization_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "coaches", ["organization_id"], name: "index_coaches_on_organization_id", using: :btree
 
   create_table "organizations", force: true do |t|
     t.string   "name"
---
+++ spec/models/repo/coach_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Repo::Coach do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:organization_id) }
+  it { should belong_to(:organization) }
+end
