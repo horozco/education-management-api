8ca923fb444e3b559c2e4dddf648d556db80bdb3
Edgar Ortega <edgar.ortega@koombea.com>
2014-06-26 14:40:08 -0500

    Remove API constraints

--- lib/api_constraints.rb
+++
@@ -1,9 +0,0 @@
-class ApiConstraints
-  def initialize(options)
-    @version, @default = options.values_at(:version, :default)
-  end
-
-  def matches?(req)
-    @default || req.headers['Accept'].include?("application/vnd.example.v#{@version}")
-  end
-end
